56f1938e52d4cec89d621393f0a6f9db620e79a4
James Espie <jamesespie@gmail.com>
2023-07-07 17:03:48 +1200

    Take out quotes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('Headline should say "Kitchen Stink"', () => {
+  it('Headline should say Kitchen Stink', () => {
     cy.visit('https://example.cypress.io')
     cy.get('h1').should('contain','Kitchen Stink')
   })
